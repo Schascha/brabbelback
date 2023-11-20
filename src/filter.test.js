4715a1ed0ca703c389395910ec8bfd53f2a93ef5
Sascha Künstler <sascha.kuenstler@moccu.com>
2023-11-20 11:11:03 +0100

    Update tests

--- src/filter.test.js
+++ src/filter.test.js
@@ -1,34 +1,72 @@
-import { compare, groupBy } from './filter';
+import { compare, filter, groupBy } from './filter';
 
 describe('The filter utils', () => {
 	it('should compare values', () => {
 		const array = ['foo', 'bar', 'baz'];
-
 		expect(compare()).toBeUndefined();
 		expect(compare(array, 'foo', 'contains')).toBeTruthy();
 		expect(compare('foobar', 'foo', 'contains')).toBeTruthy();
 		expect(compare(array, 'foo')).toBeTruthy();
 	});
 
+	it('should filter an array of objects with multiple criteria', () => {
+		const array = [
+			{
+				name: 'Audi',
+				type: 'car',
+				color: 'red'
+			},
+			{
+				name: 'VW',
+				alias: 'Volkswagen',
+				type: 'car',
+				color: 'black'
+			},
+			{
+				name: 'Suziki',
+				type: 'motorcycle',
+				color: 'blue'
+			},
+		];
+		expect(filter(array)).toBe(array);
+		expect(filter(array, [])).toBe(array);
+		expect(filter(array, [
+			{
+				field: 'type',
+				value: 'car',
+			},
+			{
+				field: 'color',
+				value: 'black',
+			}
+		]).length).toBe(1);
+		expect(filter(array, [
+			{
+				field: ['name', 'alias'],
+				value: 'VW',
+			},
+		]).length).toBe(1);
+	});
+
 	it('should group array items by key', () => {
 		const array = [
-				{
-					group: 'foo',
-					name: 'Item 1',
-				},
-				{
-					group: 'bar',
-					name: 'Item 2',
-				},
-				{
-					group: 'foo',
-					name: 'Item 3',
-				},
-				{
-					name: 'Item 4',
-				},
-			],
-			obj = groupBy(array, 'group');
+			{
+				group: 'foo',
+				name: 'Item 1',
+			},
+			{
+				group: 'bar',
+				name: 'Item 2',
+			},
+			{
+				group: 'foo',
+				name: 'Item 3',
+			},
+			{
+				name: 'Item 4',
+			},
+		];
+		const obj = groupBy(array, 'group');
 		expect(obj.foo).toBeDefined();
 		expect(obj.foo.length).toBe(2);
 		expect(obj.bar).toBeDefined();
