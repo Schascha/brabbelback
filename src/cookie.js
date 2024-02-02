8081fc8f92be14bb470da9d10d209b6773320651
Sascha Künstler <sascha.kuenstler@moccu.com>
2024-02-02 13:30:09 +0100

    Update setCookie string

--- src/cookie.js
+++ src/cookie.js
@@ -22,8 +22,7 @@ export function setCookie(name, value, days = 1) {
 
 	const date = new Date();
 	date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
-	document.cookie =
-		name + '=' + value + '; expires=' + date.toUTCString() + '; path=/';
+	document.cookie = `${name}=${value}; expires=${date.toUTCString()}; path=/;`;
 }
 
 /**
