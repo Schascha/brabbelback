b53be2d8b4ddad0b123d11eef5821bb4bd3b390a
Sascha Künstler <sascha.kuenstler@moccu.com>
2024-10-07 11:50:59 +0200

    Update eslint

--- eslint.config.js
+++ eslint.config.js
@@ -2,12 +2,14 @@ const globals = require('globals');
 const jest = require('eslint-plugin-jest');
 const js = require('@eslint/js');
 const schascha = require('@schascha/eslint-config');
+const tseslint = require('typescript-eslint');
 
 module.exports = [
 	js.configs.recommended,
 	jest.configs['flat/recommended'],
+	...tseslint.configs.recommended,
 	{
-		ignores: ['node_modules/**', 'coverage/**'],
+		ignores: ['node_modules/**', 'coverage/**', 'dist/**'],
 	},
 	{
 		languageOptions: {
@@ -20,6 +22,11 @@ module.exports = [
 		},
 		rules: {
 			...schascha.rules,
+			"@typescript-eslint/no-explicit-any": ["off"],
+			"@typescript-eslint/no-unsafe-function-type": ["off"],
+			"@typescript-eslint/no-unused-expressions": ["off"],
+			"@typescript-eslint/ban-ts-comment": ["off"],
+			"@typescript-eslint/no-require-imports": ["off"],
 		},
 	},
 ];
